8460db738a836e16f290229367e9e2a748437487
Joe <joeypompeo@gmail.com>
2021-02-11 22:15:22 -0500

    Lab 11 Movie Class Only

---
+++ Lab_11_Movie_Database/Lab_11_Movie_Database/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_11_Movie_Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movie = new Movie();
+
+            movie.GetHorror();
+        }
+    }
+}
